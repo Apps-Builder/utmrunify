9f9b77758640398330321e4462422e484b167506
Jason Pua <pzhiying0306@gmail.com>
2024-12-22 22:22:30 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:utmrunify/loginpage.dart';
 import 'event_details.dart';
 import 'track_distance.dart';
 import 'shop.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
 }
 
